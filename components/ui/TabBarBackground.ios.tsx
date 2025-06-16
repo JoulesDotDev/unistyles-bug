d558cee07412537258bd491757d4f420fb20d8dc
Joules <joules@joules.dev>
2025-06-17 01:42:40 +0200

    bug

--- components/ui/TabBarBackground.ios.tsx
+++ components/ui/TabBarBackground.ios.tsx
@@ -1,6 +1,6 @@
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
 
 export default function BlurTabBarBackground() {
   return (
---
+++ unistyles.ts
@@ -0,0 +1,22 @@
+export const lightTheme = {
+    colors: {
+        primary: '#ff1ff4',
+        secondary: '#1ff4ff'
+        // any nesting, spreading, arrays, etc.
+    },
+    // functions, external imports, etc.
+    gap: (v: number) => v * 8
+}
+
+export const otherTheme = {
+    colors: {
+        primary: '#aa12ff',
+        secondary: 'pink'
+    },
+    gap: (v: number) => v * 8
+}
+
+export const appThemes = {
+    light: lightTheme,
+    other: otherTheme
+}
