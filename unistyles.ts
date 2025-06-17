cbc7b9ba5a4b33467153544a08722cc32532e974
Joules <joules@joules.dev>
2025-06-17 02:10:04 +0200

    settings

--- unistyles.ts
+++ unistyles.ts
@@ -1,14 +1,14 @@
-export const lightTheme = {
+import { StyleSheet } from 'react-native-unistyles'
+
+const lightTheme = {
     colors: {
         primary: '#ff1ff4',
         secondary: '#1ff4ff'
-        // any nesting, spreading, arrays, etc.
     },
-    // functions, external imports, etc.
     gap: (v: number) => v * 8
 }
 
-export const otherTheme = {
+const otherTheme = {
     colors: {
         primary: '#aa12ff',
         secondary: 'pink'
@@ -16,7 +16,31 @@ export const otherTheme = {
     gap: (v: number) => v * 8
 }
 
-export const appThemes = {
+const appThemes = {
     light: lightTheme,
     other: otherTheme
 }
+
+const breakpoints = {
+    xs: 0,
+    sm: 300,
+    md: 500,
+    lg: 800,
+    xl: 1200
+}
+
+type AppBreakpoints = typeof breakpoints
+type AppThemes = typeof appThemes
+
+declare module 'react-native-unistyles' {
+    export interface UnistylesThemes extends AppThemes {}
+    export interface UnistylesBreakpoints extends AppBreakpoints {}
+}
+
+StyleSheet.configure({
+    settings: {
+        initialTheme: 'light',
+    },
+    breakpoints,
+    themes: appThemes
+})
